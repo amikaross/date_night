eb28d3cfb1a3c530044e3fc83a0bbd7aa98ab473
Amanda Ross <amikaross@gmail.com>
2023-04-24 16:16:29 -0600

    Add test for non-unique email

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -20,4 +20,25 @@ RSpec.describe "Users API" do
     expect(response.status).to eq(201)
     expect(created_user.email).to eq('amanda@example.com')
   end
+
+  it "will return an error if the email is not unique" do 
+    User.create(email: 'amanda@example.com', password: 'password')
+
+    user_params = {
+      email: 'amanda@example.com',
+      password: 'another_password'
+    }
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    expect(User.all.count).to eq(1)
+
+    post "/api/v1/users", headers: headers, params: JSON.generate(user: user_params)
+    
+    parsed_response = JSON.parse(response.body)
+
+    expect(response.status).to eq(400)
+    expect(parsed_response["message"]).to eq("Bad Request")
+    expect(parsed_response["errors"]).to eq(["Email has already been taken"])
+  end
 end
