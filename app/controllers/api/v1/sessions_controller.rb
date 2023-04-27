aef5c6cd25e27be21ed34cc7f898e808cc5f5ce0
Amanda Ross <amikaross@gmail.com>
2023-04-27 11:28:42 -0600

    return to sessions

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
+
   def create
     user = User.find_by(email: user_params[:email])
     if user && user.authenticate(user_params[:password])
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::API
+  include ActionController::Cookies
+  include ActionController::RequestForgeryProtection
+
+  protect_from_forgery with: :exception
+
+  before_action :set_csrf_cookie
+
   def current_user 
     @_current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end
+
+  private
+  
+    def set_csrf_cookie
+      cookies["CSRF-TOKEN"] = form_authenticity_token
+    end
 end
